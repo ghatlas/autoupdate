4ea457447d7e3c8b9874f93b792144abbfb1cb59
ghatlas <ghatlas@atlaslive.tech>
2024-10-11 16:10:54 +0300

    Create SyncAutoScript.ps1

---
+++ SyncAutoScript.ps1
@@ -0,0 +1,5 @@
+$AutoUPD = "C:\AutoUpdate\"
+$AutoScript = "AutoScript.ps1"
+$URI="https://github.com/ghatlas/autoupdate/raw/refs/heads/main/"
+try { 	Invoke-WebRequest -Uri $URI$AutoScript -OutFile $AutoUPD$AutoScript -ErrorAction Stop }
+catch { Write-Host "Invalid URI $URI$AutoScript for $AutoUPD$AutoScript" }
