5459f10ec3e01856d52e03850e28b7f616c14de2
ghatlas <ghatlas@atlaslive.tech>
2024-10-11 18:12:23 +0300

    Update AutoScript.ps1

--- AutoScript.ps1
+++ AutoScript.ps1
@@ -1,38 +1,25 @@
 $PkgArray = @()
 $NameArray = @()
-$AutoUPD = "C:\AutoUpdate\"
-$AutoCache = "Cache\"
 # Set URI for repo
 $URI='https://github.com/ghatlas/autoupdate/raw/refs/heads/main/'
 $URIDIR='bucket/'
+$AutoUPD = "C:\AutoUpdate\"
+$AutoCache = "Cache\"
+$AutoScript = "AutoScript.ps1"
+$SoftwareList = "software_list.txt"
 $OsqueryVersion = "5.13.1"
 $OsqueryName = "osquery-" + $OsqueryVersion + ".windows_x86_64"
 $OsqueryBin = "osquery\osqueryi.exe"
-$OsQueryURI="https://github.com/osquery/osquery/releases/download/$OsqueryVersion/osquery-$OsqueryVersion.windows_x86_64.zip"
 
-if ( -not ( Test-Path -Path $AutoUPD ) ) {
-	New-Item -Path $AutoUPD -ItemType Directory
-	Write-Output "Folder $AutoUPD created successfully!"
-}
-
-if ( -not ( Test-Path -Path $AutoUPD$AutoCache ) ) {
-	New-Item -Path $AutoUPD$AutoCache -ItemType Directory
-	Write-Output "Folder $AutoUPD$AutoCache created successfully!"
-}
-
-if ( -not ( Test-Path -Path $AutoUPD$OsqueryBin ) ) {
-	Invoke-WebRequest -Uri $OsQueryURI -OutFile $AutoUPD$AutoCache$OsqueryName".zip"
-	Expand-Archive -Force $AutoUPD$AutoCache$OsqueryName".zip" $AutoUPD$AutoCache
-	Move-Item -Path $AutoUPD$AutoCache$OsqueryName"\Program Files\osquery" -Destination $AutoUPD
-}
-
-Start-Process -FilePath "$AutoUPD$OsqueryBin" -ArgumentList '--json "SELECT install_date AS date, name AS name, version AS version FROM programs;"' -Wait -NoNewWindow -RedirectStandardOutput $AutoUPD\software_list.txt
-$PkgArray=Get-Content $AutoUPD\software_list.txt | ConvertFrom-Json
+Start-Process -FilePath "$AutoUPD$OsqueryBin" -ArgumentList '--json "SELECT install_date AS date, name AS name, version AS version FROM programs;"' -Wait -NoNewWindow -RedirectStandardOutput $AutoUPD$SoftwareList
+$PkgArray=Get-Content $AutoUPD$SoftwareList | ConvertFrom-Json
 
 Foreach ( $Pkg in $PkgArray ) {
+	# Save origin name of package
+	$PkgOrigin = $Pkg.Name
+
 	if ( $Pkg.Name.Contains(" ") ) {
 		$Pkg.Name = $Pkg.Name -replace "(\(.*\)|version|версия|x\d{2}|ru|en-us|,|v\d{1,}\.\d{1,}\.\d{1,})", ""
-
 		$Pkg.Name = $Pkg.Name -replace "(\s{2,})", " "
 		$NameArray = $Pkg.Name.Split( " " )
 		$Pkg.Name = ""
@@ -48,14 +35,18 @@ Foreach ( $Pkg in $PkgArray ) {
 
 	$PkgName = $Pkg.Name
 	try {
-		Invoke-WebRequest -Uri $URI$URIDIR$PkgName.json -OutFile $AutoUPD$AutoCache$PkgName.json -ErrorAction Stop
-		Write-Host "Download:" $URI$URIDIR$PkgName.json
+		Invoke-WebRequest -Uri $URI$URIDIR$PkgName".json" -OutFile $AutoUPD$AutoCache$PkgName".json" -ErrorAction Stop
+		Write-Host "Download:" $URI$URIDIR$PkgName".json"
+
+		$json_file = Get-Content $AutoUPD$AutoCache$PkgName".json" | ConvertFrom-Json
+		$json_file | Add-Member -MemberType NoteProperty -Name "origin_name" -Value $PkgOrigin
+		$json_file | ConvertTo-Json | Set-Content -Path $AutoUPD$AutoCache$PkgName".json"
 	}
 	catch {
 #		Write-Host "Error download $PkgName.json from $URI$URIDIR$PkgName.json"
 	}
 }
-
+exit
 Get-ChildItem -Path $AutoUPD$AutoCache -Name -Include *.json |
 	Foreach-Object {
 		$PkgPath = "$AutoUPD$AutoCache$_"
@@ -63,7 +54,6 @@ Get-ChildItem -Path $AutoUPD$AutoCache -Name -Include *.json |
 		$PkgBaseName = [System.IO.Path]::GetFileNameWithoutExtension( "$PkgPath" )
 		$PkgName = $PkgBaseName + "-" + $PkgJSON.version + "." + $PkgJSON.architecture.x64bit.type
 		$PkgURI = $PkgJSON.architecture.x64bit.url
-  		$PkgArgument = $PkgJSON.architecture.x64bit.argument
 		Write-Host "Github repo:" $PkgBaseName " - " $PkgJSON.version
 		Foreach ( $Pkg in $PkgArray ) {
 			if ( $Pkg.Name -eq $PkgBaseName ) { Write-Host "Local install:" $Pkg.Name " - " $Pkg.Version }
@@ -77,13 +67,13 @@ Get-ChildItem -Path $AutoUPD$AutoCache -Name -Include *.json |
 					if ( $PkgJSON.architecture.x64bit.type -eq "msi" ) {
 						Start-Process "msiexec.exe" -ArgumentList "/I $AutoUPD$AutoCache$PkgName /q" -Wait -NoNewWindow
 					} else {
-						Start-Process "$AutoUPD$AutoCache$PkgName" -ArgumentList $PkgArgument -Wait -NoNewWindow
+						Start-Process "$AutoUPD$AutoCache$PkgName" -ArgumentList "/SILENT" -Wait -NoNewWindow
 					}
 				}
 				catch {
 					Write-Host "Error install or update" $AutoUPD$AutoCache$PkgName
 				}
-    				Remove-Item $AutoUPD$AutoCache + "*" -Force -Recurse
+				Remove-Item $AutoUPD$AutoCache + "*" -Force -Recurse
 			}
 		}
 	}
